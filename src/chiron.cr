39e9a942bd5b9c84ecfebfa826961c7cae0a7758
Sam Johnson <sam@durosoft.com>
2020-03-16 02:13:34 -0400

    simple directory detection

--- src/chiron.cr
+++ src/chiron.cr
@@ -3,4 +3,22 @@ require "css-minifier"
 require "js-minifier"
 
 module Chiron
+  @@path : String = "."
+  @@html_dir : String? = nil
+  @@css_dir : String? = nil
+  @@js_dir : String? = nil
+
+  def self.load_project(@@path = ".")
+    html_dir = Path[@@path].join("html").to_s
+    css_dir = Path[@@path].join("css").to_s
+    js_dir = Path[@@path].join("js").to_s
+    @@html_dir = File.exists?(html_dir) ? html_dir : nil
+    @@css_dir = File.exists?(css_dir) ? css_dir : nil
+    @@js_dir = File.exists?(js_dir) ? js_dir : nil
+    puts @@html_dir
+    puts @@css_dir
+    puts @@js_dir
+  end
 end
+
+Chiron.load_project
