66e31d585879eb2194a9f1b8af2e43e8fd2df48a
Sam Johnson <sam@durosoft.com>
2020-03-23 02:44:54 -0400

    add CLI skeleton

--- src/cli.cr
+++ src/cli.cr
@@ -1,4 +1,19 @@
 require "./chiron"
 
 class Chiron::CLI
+  @@autorun : Bool = true
+
+  def autorun?
+    @@autorun
+  end
+
+  def autorun=(val)
+    @@autorun = val
+  end
+
+  def initialize
+    puts "hello world"
+  end
 end
+
+Chiron::CLI.new if Chiron::CLI.autorun?
