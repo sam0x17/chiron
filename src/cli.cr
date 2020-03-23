3d1116631890fd275bbb5be79489ee213175998e
Sam Johnson <sam@durosoft.com>
2020-03-23 03:10:52 -0400

    add usage information

--- src/cli.cr
+++ src/cli.cr
@@ -2,6 +2,7 @@ require "./chiron"
 
 class Chiron::CLI
   @@autorun : Bool = true
+  VERSION = "0.1.0"
 
   def self.autorun?
     @@autorun
@@ -12,7 +13,36 @@ class Chiron::CLI
   end
 
   def initialize
-    puts "hello world"
+    print_usage
+  end
+
+  def print_usage
+    puts ""
+    puts "Chiron #{VERSION} | https://github.com/sam0x17/chiron"
+    puts ""
+    puts "usage: chiron [subcommand] (args..)"
+    puts ""
+    puts "subcommands:"
+    puts ""
+    puts "  - chiron init [name]"
+    puts ""
+    puts "    * if name is specified, creates a new project with"
+    puts "      default directories (css/html/js) and the"
+    puts "      specified project name"
+    puts "    * otherwise simply creates chiron config file in"
+    puts "      the current directory"
+    puts ""
+    puts "  - chiron watch [--port 3000]"
+    puts ""
+    puts "    * runs the local development server"
+    puts "    * default port is 3000"
+    puts "    * must be executed from a chiron project directory"
+    puts ""
+    puts "  - chiron deploy"
+    puts ""
+    puts "    * deploys the current project based on config"
+    puts "    * must be executed from a chiron project directory"
+    puts ""
   end
 end
 
